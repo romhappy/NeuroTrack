084b7279bd1008eb5c5944f9b6d393c977a98e35
romhappy <romainbesin21@gmail.com>
2023-12-16 14:46:44 +0100

    feat: init commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex column flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+    <p>la teub</p>
+    <div class="flex">
+      <ExampleCard />
+      <ExampleCard />
+      <ExampleCard />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import ExampleCard from 'src/components/ExampleCard.vue';
+export default defineComponent({
+  name: 'IndexPage',
+  components: { ExampleCard }
+})
+</script>
