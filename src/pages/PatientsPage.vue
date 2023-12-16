6cbe8bf65c1b9911817d4064f0fc11cceb0867ca
romhappy <romainbesin21@gmail.com>
2023-12-16 15:30:02 +0100

    feat: add PatientsPage.vue

---
+++ src/pages/PatientsPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-page class="flex column flex-center">
+        <p>Patients</p>
+        <div class="flex">
+            <ExampleCard />
+            <ExampleCard />
+            <ExampleCard />
+        </div>
+    </q-page>
+</template>
+  
+<script>
+import { defineComponent } from 'vue';
+import ExampleCard from 'src/components/ExampleCard.vue';
+export default defineComponent({
+    name: 'IndexPage',
+    components: { ExampleCard }
+})
+</script>
+  
