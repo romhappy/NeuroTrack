64278156ac847ce0410adc937073567990f41803
romhappy <romainbesin21@gmail.com>
2023-12-16 15:14:16 +0100

    fix: remove quasar image

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header>
-          Essential Links
+          Menu
         </q-item-label>
 
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
 <template>
   <q-page class="flex column flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
     <p>la teub</p>
     <div class="flex">
       <ExampleCard />
